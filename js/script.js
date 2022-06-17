418cbe844d1e5ceb49a6f1c4e9ea6adce14c04df
Neo-Machina <andreistatache1997@gmail.com>
2022-06-17 17:50:15 +0200

    step 1 completed

--- js/script.js
+++ js/script.js
@@ -3,3 +3,37 @@
 // Aggiungere un file js in cui definire un array di oggetti che rappresentano i membri del team. 
 // Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
 // Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.
+
+// Array di oggetti che rappresentano i membri del team
+const teamMembers = [
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo'
+    },
+    {
+        name: 'Angela Caroll',
+        job: 'Chief Editor',
+        image: 'angela-caroll-chief-editor'
+    },
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        image: 'walter-gordon-office-manager'
+    },
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Developer',
+        image: 'scott-estrada-developer'
+    },
+    {
+        name: 'Barbara Ramost',
+        job: 'Graphic Designer',
+        image: 'wayne-barnett-founder-ceo'
+    }
+];
